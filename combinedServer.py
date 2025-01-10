30029849946cd14a64dad3ac9e192917f47009e0
Tirthajit Boral <121869956+Tirthajit2003@users.noreply.github.com>
2025-01-10 14:46:18 +0530

    Update combinedServer.py

--- combinedServer.py
+++ combinedServer.py
@@ -154,5 +154,5 @@ def get_pothole_prediction():
         return jsonify({'message': 'No new pothole prediction available'}), 200
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=8000)
